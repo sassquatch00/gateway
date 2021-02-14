90dd2fc35903416f8d1445cc34bd3c5217aee1f0
sassquatch00 <justinawong.2019@sis.smu.edu.sg>
2021-02-14 21:02:03 +0800

    added tcp example

--- client.py
+++ client.py
@@ -4,33 +4,52 @@ import time
 from datetime import datetime 
 from serial import Serial 
 
-TCP_IP = '192.168.1.219' # this IP of my pc. When I want raspberry pi 2`s as a client, I replace it with its IP '169.254.54.195'
-TCP_PORT = 5005
-BUFFER_SIZE = 1024
+# TCP_IP = '192.168.1.219' # this IP of my pc. When I want raspberry pi 2`s as a client, I replace it with its IP '169.254.54.195'
+# TCP_PORT = 5005
+# BUFFER_SIZE = 1024
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((TCP_IP, TCP_PORT))
+# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s.connect((TCP_IP, TCP_PORT))
 
-nextCompassPoll = 0.0
+nextCompassPoll = 0.0 
 
-serialDevDir='/dev/serial/by-id' # directory containing microbit device file link
+serialDevDir='/dev/serial/by-id' 
 
-if (os.path.isdir(serialDevDir)):
+if ( os.path.isdir(serialDevDir) ):
     serialDevices = os.listdir(serialDevDir) 
 
-    if (len(serialDevices) > 0):
+    if ( len(serialDevices) > 0 ):
 
         serialDevicePath = os.path.join(serialDevDir, serialDevices[0])
 
         serial = Serial(port=serialDevicePath, baudrate=19200, timeout=0.2) 
 
-        while(True):
+        while( True ):
 
-            receivedMsg = uart.readline()
-            print(receivedMsg)
-            s.send(receivedMsg)
-            data = s.recv(BUFFER_SIZE)
-            print ("received data:", data)
+            receivedMsg = serial.readline() 
+
+            if ( (len(receivedMsg) >= 4) and (receivedMsg[3] == b':'[0])):
+
+                msgType = receivedMsg[0:3] 
+                msgData = receivedMsg[4:]
+
+                if ( msgType == b'TIM' ):
+                    timeString = datetime.now().strftime('%H:%M') 
+                    sendMsg = b'TIM:' + timeString.encode('ascii')
+                    serial.write(sendMsg + b'\n')
+
+                elif ( msgType == b'DAT' ):
+                    dateString = datetime.now().strftime('%d-%b-%Y') 
+                    sendMsg = b'DAT:' + dateString.encode('ascii')
+                    serial.write(sendMsg + b'\n')
+
+                elif ( msgType == b'CMP' ):
+                    print('Compass Bearing = ' + msgData.decode('ascii'))
+
+            currentTime = time.time() 
+            if ( currentTime > nextCompassPoll ):
+                serial.write(b'CMP:\n')
+                nextCompassPoll = currentTime + 2.0
     else:
 
         print('No serial devices connected') 
@@ -38,3 +57,8 @@ if (os.path.isdir(serialDevDir)):
 else:
 
     print(serialDevDir + ' does not exist') 
+
+# print(receivedMsg)
+# s.send(receivedMsg)
+# data = s.recv(BUFFER_SIZE)
+# print ("received data:", data)
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 
 TCP_IP = '192.168.1.219' # this IP of my pc. When I want raspberry pi 2`s as a server, I replace it with its IP '169.254.54.195'
-TCP_PORT = 5005
+TCP_PORT = 5006
 BUFFER_SIZE = 20 # Normally 1024, but I want fast response
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,3 +15,4 @@ while 1:
     if not data: break
     print ("received data:", data)
     conn.send(data)  # echo
+    conn.close()
